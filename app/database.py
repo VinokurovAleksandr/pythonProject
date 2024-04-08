a78bcc8ec169b1e0577bda5740e0cff7ff480218
alex <a0934438949@gmail.com>
2024-04-08 21:13:54 +0300

    add description func

--- app/database.py
+++ app/database.py
@@ -1,9 +1,9 @@
-import pyodbc
-import aioodbc
 from typing import List
 from typing import Union
 
-import task
+import aioodbc
+import pyodbc
+
 from task import Task
 
 server = '127.0.0.1,1433'
@@ -13,6 +13,7 @@ password = 'P@s5w0rd'
 driver = '{ODBC Driver 18 for SQL Server}'
 
 table_name = "Tasks"
+
 
 async def get_connection() -> aioodbc.Connection:
     conn = await aioodbc.connect(
@@ -27,6 +28,10 @@ async def get_connection() -> aioodbc.Connection:
 
 
 async def create_database(database_name: str) -> None:
+    '''
+    Creates the database
+    :param database_name: Name of the database
+    '''
     conn = await get_connection()
     cursor = conn.cursor()
     try:
@@ -37,20 +42,31 @@ async def create_database(database_name: str) -> None:
     finally:
         conn.close()
 
+
 async def create_task(task: Task) -> Task:
+    '''
+    Create a task in the database
+    :param task: The task object to be created
+    :return: The created task object
+    '''
     async with await get_connection() as conn:
         cursor = await conn.cursor()
         await cursor.execute(
             f'INSERT INTO {table_name} (id, name, status, term, description) VALUES (?, ?, ?, ?, ?)',
-                        (task.id,
-                         task.name,
-                         task.status,
-                         task.term,
-                         task.description))
+            (task.id,
+             task.name,
+             task.status,
+             task.term,
+             task.description))
         await conn.commit()
     return task
 
+
 async def fetch_task() -> List[Task]:
+    '''
+    Fetch task in the database
+    :return: List[Task] A list of task objects
+    '''
     async with await get_connection() as conn:
         cursor = await conn.cursor()
         await cursor.execute(f'SELECT * FROM {table_name}')
@@ -63,16 +79,27 @@ async def fetch_task() -> List[Task]:
             for row in tasks]
 
 
-
 async def update_task(task_id: Union[int, str], task: Task) -> Task:
+    '''
+    Update a task in the database
+    :param task_id: Id of the task to be updated
+    :param task: The task to be updated
+    :return:  The updated task object
+    '''
     async with await get_connection() as conn:
         cursor = await conn.cursor()
         await cursor.execute(f'UPDATE {table_name} SET name = ?, status = ?, term = ?, description = ? WHERE id = ?',
-                       (task.name, task.status, task.term, task.description, task_id))
+                             (task.name, task.status, task.term, task.description, task_id))
         await cursor.commit()
     return task
 
+
 async def delete_task(task_id: int) -> Task:
+    '''
+    Delete a task in the database
+    :param task_id: Id of the task to be deleted
+    :return:  The deleted task object
+    '''
     async with await get_connection() as conn:
         cursor = await conn.cursor()
         await cursor.execute(f'DELETE FROM {table_name} WHERE id = ?',
@@ -80,21 +107,12 @@ async def delete_task(task_id: int) -> Task:
         await cursor.commit()
     return Task(id=task_id, name='', status='', term='', description='')
 
-
 # Create BD
-#cursor.execute('CREATE DATABASE mydatabase')
-#print("Database created successfully")
+# cursor.execute('CREATE DATABASE mydatabase')
+# print("Database created successfully")
 
-#Create TABLE
+# Create TABLE
 
-#cursor.execute('CREATE TABLE Tasks ( ID INT PRIMARY KEY NOT NULL, Name VARCHAR(50) NOT NULL, Status VARCHAR(50), Term VARCHAR(50), Description VARCHAR(255))')
-#cursor.commit()
-#print("Table Created")
-
-
-
-
-
-
-
-
+# cursor.execute('CREATE TABLE Tasks ( ID INT PRIMARY KEY NOT NULL, Name VARCHAR(50) NOT NULL, Status VARCHAR(50), Term VARCHAR(50), Description VARCHAR(255))')
+# cursor.commit()
+# print("Table Created")
--- app/main.py
+++ app/main.py
@@ -1,14 +1,22 @@
-from fastapi import FastAPI, HTTPException, status
+import tracemalloc
 from typing import Union
+
+from fastapi import FastAPI, HTTPException, status
 
 from database import fetch_task, create_task, update_task, delete_task
 from task import Task
-import tracemalloc
 
 app = FastAPI()
 
+
 @app.post("/task/", response_model=Task)
 async def add_task(task: Task) -> Task:
+    '''
+    Endpoint for adding a new task.
+    :param: task: Task object to be added.
+    :return: The added task object.
+    :raise:  If an error occurs while creating the task.
+    '''
     try:
         await create_task(task)
         return task
@@ -16,8 +24,17 @@ async def add_task(task: Task) -> Task:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
                             detail=str(e))
 
+
 @app.get("/task/{id}", response_model=Task)
 async def get_task(task_id: int) -> Task:
+    '''
+    Getting a task by its id.
+    :param task_id: id of the task.
+    :return: Task object with given id.
+    '''
+    if task_id <= 0:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail="Task ID must be greater than 0")
     tasks = await fetch_task()
     for task in tasks:
         if task.id == task_id:
@@ -25,10 +42,19 @@ async def get_task(task_id: int) -> Task:
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                         detail="Task not found")
 
+
 @app.put("/task/{id}", response_model=Task)
 async def update_task_id(task_id: Union[int, str], task: Task) -> Task:
+    '''
+    Updating a task by its id.
+    :param task_id: id of the task.
+    :param task: Task object to be updated.
+    :return: The update task object by its id.
+    :raise: If the task with the specified is not found or
+            no change were made during the update
+    '''
     if task_id <= 0:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
                             detail="Task ID must be greater than 0")
     try:
         update_rows = await update_task(task_id, task)
@@ -41,10 +67,18 @@ async def update_task_id(task_id: Union[int, str], task: Task) -> Task:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
                             detail=str(e))
 
+
 @app.delete("/task/{task_id}", response_model=Task)
 async def delete_task_id(task_id: int) -> Task:
+    '''
+    Deleting a task by its id
+    :param task_id(int): The id of the task to be delete
+    :return: The  deleted task object.
+    :raise: If the task with the specified is not found or
+            ID must be greater than 0
+    '''
     if task_id <= 0:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
                             detail="Task ID must be greater than 0")
     tasks = await fetch_task()
     for task in tasks:
